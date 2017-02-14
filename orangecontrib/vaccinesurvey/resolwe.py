fb7a4a4f619e5cc5357d1951fca197c579a034bf
JakaKokosar <jaka.kokosar@gmail.com>
2017-02-14 15:24:12 +0100

    change variable types

--- orangecontrib/vaccinesurvey/resolwe.py
+++ orangecontrib/vaccinesurvey/resolwe.py
@@ -1,40 +1,55 @@
 """Resolwe api"""
 import requests
+import datetime
 
 
 from resdk import Resolwe
-from Orange.data import ContinuousVariable, StringVariable, Domain, Table
+from Orange.data import ContinuousVariable, StringVariable, TimeVariable, DiscreteVariable, Domain, Table
 
 
-DATA = [
-        ('Ama1', 'ama1'),
-        ('Msp1', 'msp1'),
-        ('Msp2', 'msp2'),
-        ('Nanp', 'nanp'),
-        ('Total ige', 'total_ige')]
-METAS = [
-        ('Study code', 'study_code'),
-        ('Birth date', 'birth_date'),
-        ('Sex', 'sex'),
-        ('Ethnicity', 'ethnicity'),
-        ('Entry Date', 'entry_date'),
-        ('Village Code', 'village_code')]
+IMMUNOLOGICAL_DATA = [('Ama1', 'ama1'), ('Msp1', 'msp1'), ('Msp2', 'msp2'),
+                      ('Nanp', 'nanp'), ('Total ige', 'total_ige')]
+
+DATE = [('Birth date', 'birth_date'), ('Entry Date', 'entry_date')]
+METAS = [('Study code', 'study_code')]
+DISCRETE = [('Sex', 'sex'), ('Ethnicity', 'ethnicity'), ('Village Code', 'village_code')]
 
 
-def _parse_descriptor(descriptor):
-    metas = [descriptor[value[1]] for value in METAS if type(descriptor[value[1]]) is not dict]
-    data = [descriptor['immunological_data'][value[1]] for value in DATA]
-    return data + metas
+def _add_discrete_value(variable, val):
+    """ Checks if item is in values. If its not it will add it """
+    if val not in variable.values:
+        variable.add_value(val)
 
 
 def to_orange_table(samples):
-    variables = [ContinuousVariable.make(var[0]) for var in DATA]
+    #  Create variables
+    immuno_data = [ContinuousVariable.make(var[0]) for var in IMMUNOLOGICAL_DATA]
+    discrete_vars = [DiscreteVariable.make(var[0]) for var in DISCRETE]   # we add values later
+    date_vars = [TimeVariable.make(var[0]) for var in DATE]
+    #  Create metas
     meta_attrs = [StringVariable.make(meta[0]) for meta in METAS]
-    domain = Domain(variables, metas=meta_attrs)
-    table = []
 
+    #  Parse sample descriptor
+    def _parse_descriptor(descriptor):
+        """ return a list of values from sample descriptor """
+        metas = [descriptor[value[1]] for value in METAS]
+        immuno_data = [descriptor['immunological_data'][value[1]] for value in IMMUNOLOGICAL_DATA]
+        discrete_data = [str(descriptor[value[1]]) for value in DISCRETE]
+        date_data = [str(datetime.datetime.strptime(descriptor[value[1]], "%Y-%m-%d").date()) for value in DATE
+                     if descriptor[value[1]]]
+
+        # add all possible values from descriptor to discrete variables
+        for value in DISCRETE:
+            [_add_discrete_value(var,  str(descriptor[value[1]])) for var in discrete_vars if var.name == value[0]]
+
+        return immuno_data + discrete_data + date_data + metas
+
+    #  Create table
+    table = []
     for sample in samples:
         table.append(_parse_descriptor(sample.descriptor['sample']))
+    #  Create domain
+    domain = Domain(immuno_data + discrete_vars + date_vars, metas=meta_attrs)
 
     return Table(domain, table)
 
